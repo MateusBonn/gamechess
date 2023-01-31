c39221621457e5008da6a0d87200eab83c1f49d7
Mateus Bonn Ferreira <mateusbonn.mbf@gmail.com>
2023-01-31 20:06:55 -0300

    FAZENDO O BOARD DO JOGO

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position place = new Position(3,5);
-		System.out.println(place);
 		
-		Board board = new Board (8, 8);
-		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
