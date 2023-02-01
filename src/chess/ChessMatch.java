9e2722fe3827fff9e6ae7d050832526739627f71
Mateus Bonn Ferreira <mateusbonn.mbf@gmail.com>
2023-01-31 21:33:20 -0300

    MAKING EXCEPTIONS

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,7 +29,7 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.PlacePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.PlacePiece(new Rook(board, Color.WHITE), new Position(1, 1));
 		board.PlacePiece(new King(board, Color.BLACK), new Position(0, 4));
 	}
 
