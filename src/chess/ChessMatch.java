78fe888dc53f111edb76d620a0f4cc1ba7f5b875
Mateus Bonn Ferreira <mateusbonn.mbf@gmail.com>
2023-02-17 06:51:02 -0300

    put moviment in some pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -33,6 +33,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target =  targetPosition.toPosition();	
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -43,6 +44,12 @@ public class ChessMatch {
 		}
 		if (board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There's no possible moves for chosen piece");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException ("The chosen piece cannot move to target");
 		}
 	}
 	
@@ -57,6 +64,8 @@ public class ChessMatch {
 	private void placeNewPiece (char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
+	
 	
 	private void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
